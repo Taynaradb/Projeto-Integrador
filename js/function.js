a551f9e294693bd224e620bd024314585e15798f
taynaradb <taynaraax0@hotmail.com>
2021-03-09 22:12:05 -0300

    new pages and adjustments

--- js/function.js
+++ js/function.js
@@ -1,31 +1,12 @@
-//function to get rid of preloader
+// PRELOADER
 
 window.addEventListener('load', () => {
     const preload = document.querySelector(".preload");
     preload.classList.add("preload-finish");    
 });
 
-//end of preloader function
+// PRELOADER
 
-//nav bar
+//GALLERY
 
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('nav-links li');
-
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-        navLinks.forEach((link, index) => {
-            if(link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 5}s';
-            } 
-    });
-  burger.classList.toggle('toggle');
-});
-
-}
-    navSlide();
+      //GALLERY

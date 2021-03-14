13206438da2a6062d3dddf5f37920a6b90b7caf3
taynaradb <taynaraax0@hotmail.com>
2021-03-14 20:00:23 -0300

    readjusting

--- js/function.js
+++ js/function.js
@@ -2,13 +2,65 @@
 
 window.addEventListener('load', () => {
     const preload = document.querySelector(".preload");
-    preload.classList.add("preload-finish");    
+    preload.classList.add("preload-finish");
 });
 
 // PRELOADER
 
-//GALLERY
+//SPONSER
+$('.owl-one').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
 
-//GALLERY
+//SPONSER
+ //VIDEO
 
-     
+ $('.owl-two').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
+
+$('.owl-three').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
